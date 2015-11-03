fdc3062a7ea0973938a4ea7a16b6a62e02256a20
“DovileSand” <dovile.sand@protonmail.ch>
2015-11-03 16:07:45 +0000

    Add link model

--- spec/feature/homepage_spec.rb
+++ spec/feature/homepage_spec.rb
@@ -2,6 +2,8 @@
 # So that I can quickly go to a web site I want
 # I would like to see a list of links
 
+# require 'link'
+
 feature "On my homepage" do
   scenario "I can see list of the links" do
     Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
@@ -12,6 +14,6 @@ feature "On my homepage" do
     within 'url#links' do
       expect(page).to have_content('Makers Academy')
     end
-    
+
   end
 end
