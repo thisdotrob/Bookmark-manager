fff55ebbb103ba87c99564f4391af4a26b82cb7f
thisdotrob <thisdotrob@gmail.com>
2015-11-06 19:34:37 +0000

    Change name of password confirmation field

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -3,6 +3,6 @@ def sign_up
   fill_in 'username', with: 'user1'
   fill_in 'email', with: 'user1@users.com'
   fill_in 'password', with: 'password1'
-  fill_in 'password_check', with: 'password1'
+  fill_in 'password_confirmation', with: 'password1'
   click_button 'Submit'
 end
